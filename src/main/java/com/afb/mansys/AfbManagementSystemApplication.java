d17f53ac530d4f7e65fd2e76359f66211caf50f8
mvvassilev <m.v.vassilev@abv.bg>
2020-01-21 18:11:44 +0200

    initial commit

---
+++ src/main/java/com/afb/mansys/AfbManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.afb.mansys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfbManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfbManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afb/mansys/AfbManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afb.mansys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfbManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
