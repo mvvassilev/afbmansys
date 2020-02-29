4be7133b17e99f362e60ab772b11c20593034e78
mvvassilev <m.v.vassilev@abv.bg>
2020-02-29 14:05:09 +0200

    CrossOrigin annotation for "http://localhost:4200" added to MemberController

--- src/main/java/com/afb/mansys/api/MemberController.java
+++ src/main/java/com/afb/mansys/api/MemberController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@CrossOrigin("http://localhost:4200")
 @RequestMapping("members")
 @RestController
 public class MemberController {
